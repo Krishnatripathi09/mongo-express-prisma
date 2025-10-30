ba5af85c68e2d9b5979d8232ec15d6f806883ae0
Krishnatripathi09 <192.168.9.99kj@gmail.com>
2025-10-30 18:50:34 +0530

    feat: Added a get Route

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is Listening on PORT ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.status(400).send("I am a get Route");
+});
